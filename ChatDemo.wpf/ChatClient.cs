68df2c91ce8c61906671d7e8edb0e993112e2305
Kelly E. Osoba <sirkellyc@gmail.com>
2023-10-06 12:00:28 +0100

    Added Client side

---
+++ ChatDemo.wpf/ChatClient.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+public class ChatClient
+{
+    private HubConnection _connection;
+
+    public async Task StartConnectionAsync()
+    {
+        _connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7254/group-notification")
+            .Build();
+
+        await _connection.StartAsync();
+
+        _connection.On<object>("ReceiveMessage", message =>
+        {
+            // Handle received messages here
+            Console.WriteLine($"Received message: {message}");
+        });
+    }
+
+    public async Task JoinGroupAsync(string groupName)
+    {
+        await _connection.InvokeAsync("JoinGroup", groupName);
+    }
+
+    public async Task LeaveGroupAsync(string groupName)
+    {
+        await _connection.InvokeAsync("LeaveGroup", groupName);
+    }
+
+    public async Task SendMessageToGroupAsync(string groupName, string message)
+    {
+        await _connection.InvokeAsync("SendMessageToGroup", groupName, message);
+    }
+}
---
+++ ChatDemo.wpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatDemo.wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ChatClient _chatClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _chatClient = new ChatClient();
+            InitializeConnection();
+        }
+
+        private async void InitializeConnection()
+        {
+            await _chatClient.StartConnectionAsync();
+        }
+
+        private async void JoinGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            string groupName = GroupNameTextBox.Text;
+            await _chatClient.JoinGroupAsync(groupName);
+        }
+
+        private async void LeaveGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            string groupName = GroupNameTextBox.Text;
+            await _chatClient.LeaveGroupAsync(groupName);
+        }
+
+        private async void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            string groupName = GroupNameTextBox.Text;
+            string message = MessageTextBox.Text;
+            await _chatClient.SendMessageToGroupAsync(groupName, message);
+        }
+    }
+
+}
